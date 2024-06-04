3d63456fb54f1178d4c7afc6f5401f85d19cc1b7
lev cherepanov <lev.cherepanov2001@yandex.ru>
2024-06-04 09:52:22 +0500

    убрал пинию

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import './assets/styles/main.scss';
 
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import { initRouter } from './router';
 
 const app = createApp(App);
 
-app.use(createPinia());
 app.use(initRouter());
 
 app.mount('#app');
